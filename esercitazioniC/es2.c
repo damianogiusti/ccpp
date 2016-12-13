09943e48a5954f01146ed690d57d2bef897d5586
Daniele Angeli <daniele.angeli@molo17.com>
2016-12-13 14:45:50 +0100

    porting to c++

--- esercitazioniC/es2.c
+++ esercitazioniC/es2.c
@@ -2,17 +2,20 @@
 // Created by Damiano Giusti on 26/10/16.
 //
 
-#include <stdio.h>
+#include <iostream>
+
+unsing namespace std;
 
 int main() {
-
+    
     int array[] = { 35, 46, 77, 11, 9, 0 };
     int length = 6;
     int i = 0, sum = 0;
     for (i = 0; i < length; i++)
         sum += array[i];
-
-    printf("sum of array is: %d\n", sum);
-
+    
+    cout<<"sum of array is: "<<sum<<endl;
+    //    printf("sum of array is: %d\n", sum);
+    
     return 0;
 }
